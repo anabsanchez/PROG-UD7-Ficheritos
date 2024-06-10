a64721b9b158077e383793bce64533622ef89a6d
anabsanchez <usr2015@salesianos-lacuesta.net>
2024-06-10 20:35:25 +0100

    feat: Create output file and length validation

---
+++ src/net/salesianos/activity1/Main.java
@@ -0,0 +1,41 @@
+package net.salesianos.activity1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String fileURL = "src/files/output/file.txt";
+
+        String text = getText();
+    }
+
+    private static String getText() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = "";
+        boolean isActive = true;
+
+        System.out.println("Introduzca un texto de al menos 30 caracteres: ");
+
+        while (isActive) {
+            
+            text = scanner.nextLine();
+
+            int textLength = text.length();
+            isActive = textLength < 30;
+
+            if (isActive) {
+                System.out.println("Texto no válido. Faltan " + (30 - textLength) + " caracteres.");
+            }
+        }
+
+        scanner.close();
+        return text;
+    }
+}
+
+
+
